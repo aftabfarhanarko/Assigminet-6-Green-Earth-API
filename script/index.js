7f2aa893d5260f15b0785d743772579f9596ddf4
Aftab Farhan <aftabfarhan324@gmail.com>
2025-09-08 18:49:09 +0600

    addes files

--- script/index.js
+++ script/index.js
@@ -9,11 +9,12 @@ const addCarrigoy = (cata) => {
   const cointnars = document.getElementById("titleCointnar");
   cointnars.innerHTML = "";
   cata.forEach((element) => {
-    const adds = document.createElement("div");
+    const adds = document.createElement("ul");
     adds.innerHTML = `
-         <div class="mt-3  font-medium hover:bg-[#15803D] hover:text-white rounded-md">
-            <li id="${element.id}" class="list-none py-2 px-2 cursor-pointer"><a>${element.category_name}</a></li>
-         </div>
+         <ul class="mt-3 font-medium hover:bg-[#15803D] hover:text-white rounded-md  flex">
+            <li id="${element.id}" class="list-none py-2 px-5 cursor-pointer
+            "><a>${element.category_name}</a></li>
+         </ul>
         `;
     cointnars.append(adds);
   });
@@ -47,26 +48,39 @@ const cardCointener = (allImg) => {
   const cointnarsCard = document.getElementById("cardCointener");
   cointnarsCard.innerHTML = "";
   allImg.forEach((dates) => {
-    console.log(dates);
+    modalCointnar(dates.id);
     const cardes = document.createElement("div");
     cardes.innerHTML = `
-                 <div class="border-1 border-gray-300 rounded-md rounded-t-lg max-w-[650px">
+                 <div class="border-1 border-gray-300 rounded-md rounded-t-lg ">
                         <div class="bg-[#FFFFFF]  rounded-md rounded-t-lg shadow-md">
                             <img class="w-full h-[270px] rounded-t-lg cover" src="${dates.image}" alt="">
                            <div class="p-4">
-                             <h2 class="text-xl font-bold mt-3">${dates.name}</h2>
-                            <p class="">${dates.description}</p>
+                             <h2 id="modalClick" class="text-xl font-bold mt-3">${dates.name}</h2>
+                            <p class="font-medium">${dates.description}</p>
 
                              <div class="flex justify-between items-center mt-3">
                                 <button class="btn">Fruit Tree</button>
                                  <h2 class="font-bold">$ ${dates.price}</h2>
                              </div>
-                             <button class="btn w-full mt-4">Add To Card</button>
+                             <button class="btn w-full mt-6 bg-[#15803d] rounded-full text-white text-xl font-medium">Add To Card</button>
                         </div>
                            </div>
                    </div>
     `;
     cointnarsCard.append(cardes);
   });
+  // modalCointnar(allImg.id);
 };
+
+const modalCointnar = (modal) => {
+  const cointModalText = document.getElementById('modalClick');
+
+  const modalUrl = `https://openapi.programming-hero.com/api/plant/${modal}`
+  fetch(modalUrl)
+  .then(res => res.json())
+  .then(data => console.log(data));
+}
+
+
 catagori();
+

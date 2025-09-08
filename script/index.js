5ca32f745db8b12f692da11a9fd50fd8343a79d4
Aftab Farhan <aftabfarhan324@gmail.com>
2025-09-08 21:51:49 +0600

    addes total cardes

--- script/index.js
+++ script/index.js
@@ -36,7 +36,7 @@ const addCarrigoy = (cata) => {
 };
 
 const cardsApi = (id) => {
-  console.log(id);
+  // console.log(id);
   const url = ` https://openapi.programming-hero.com/api/category/${id}`;
   fetch(url)
     .then((res) => res.json())
@@ -44,7 +44,7 @@ const cardsApi = (id) => {
 };
 
 const cardCointener = (allImg) => {
-  console.log(allImg);
+  // console.log(allImg);
   const cointnarsCard = document.getElementById("cardCointener");
   cointnarsCard.innerHTML = "";
   allImg.forEach((dates) => {
@@ -60,9 +60,9 @@ const cardCointener = (allImg) => {
 
                              <div class="flex justify-between items-center mt-3">
                                 <button class="btn rounded-full text-green-600 bg-[#DCFCE7] text-[19px">Fruit Tree</button>
-                                 <h2 class="font-bold"><span class="text-1xl font-extrabold">৳</span> ${dates.price}</h2>
+                                 <h2 class="font-bold"><span class="text-1xl font-extrabold">৳</span> <span>${dates.price}</span></h2>
                              </div>
-                             <button class="btn w-full mt-6 bg-[#15803d] rounded-full text-white text-xl font-medium">Add To Card</button>
+                             <button id="addNow" class="btn w-full mt-6 bg-[#15803d] rounded-full text-white text-xl font-medium addCardBtns">Add To Card</button>
                         </div>
                            </div>
                    </div>
@@ -70,17 +70,41 @@ const cardCointener = (allImg) => {
     cointnarsCard.append(cardes);
   });
   // modalCointnar(allImg.id);
+  sideBar(allImg);
 };
 
+const mainCointnar = document.getElementById("cardCointener");
+mainCointnar.addEventListener("click", (e) => {
+  if (e.target.innerText === "Add To Card") {
+    const idName = e.target.parentNode.children[0].innerText;
+    const idPrice = e.target.parentNode.children[2].children[1].children[1].innerText;
+   
+    const total = document.getElementById("clearBtn");
+    console.log(total);
+
+
+    const divPush = document.getElementById("newaddesDiv");
+    const creatPushDiv = document.createElement("div");
+    creatPushDiv.innerHTML = `
+        <div  class="border-1 flex justify-between items-center py-5 px-3 rounded-md mt-3 bg-[#F5FFF6] border-gray-200">
+             <div class="">
+                <h2 class="text-xl font-medium">${idName}</h2>
+                <p class="mt-2 font-medium"><span class="text-1xl font-extrabold">৳${idPrice}</span>
+              </div>
+            <p id="clearBtn" class="">❌ </p>
+        </div>
+    `;
+    divPush.appendChild(creatPushDiv);
+  }
+});
+
+
 const modalCointnar = (modal) => {
-  const cointModalText = document.getElementById('modalClick');
+  const cointModalText = document.getElementById("modalClick");
 
-  const modalUrl = `https://openapi.programming-hero.com/api/plant/${modal}`
-  fetch(modalUrl)
-  .then(res => res.json())
-  .then(data => console.log(data));
-}
-
+  const modalUrl = `https://openapi.programming-hero.com/api/plant/${modal}`;
+  fetch(modalUrl).then((res) => res.json());
+  // .then(data => console.log(data));
+};
 
 catagori();
-
